c266d675da2263a828eb5d1b9bea849f6184344d
Johan Larsson <johan_gavle1@hotmail.com>
2016-06-22 15:50:15 +0200

    Initial, tested in machine.
    - Doe snot look like touch numpad is supported for Win 7.

---
+++ TouchFormBox/DummyViewModel.cs
@@ -0,0 +1,81 @@
+﻿namespace TouchFormBox
+{
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+
+    using Gu.Units;
+
+    using JetBrains.Annotations;
+
+    public class DummyViewModel : INotifyPropertyChanged
+    {
+        private string firstName;
+        private string lastName;
+        private Length length;
+        private Mass weight;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string FirstName
+        {
+            get
+            {
+                return this.firstName;
+            }
+            set
+            {
+                if (value == this.firstName) return;
+                this.firstName = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return this.lastName;
+            }
+            set
+            {
+                if (value == this.lastName) return;
+                this.lastName = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        public Length Length
+        {
+            get
+            {
+                return this.length;
+            }
+            set
+            {
+                if (value.Equals(this.length)) return;
+                this.length = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        public Mass Weight
+        {
+            get
+            {
+                return this.weight;
+            }
+            set
+            {
+                if (value.Equals(this.weight)) return;
+                this.weight = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
